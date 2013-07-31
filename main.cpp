657a11744edd96e6467073bb8ea8313357989747
Wyatt Carss <carss.w@gmail.com>
2013-07-30 20:46:06 -0400

    Pull key pressed logic out of quit-checking.

--- main.cpp
+++ main.cpp
@@ -5,16 +5,22 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Flaming Octo Avenger!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    bool key_pressed;
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
-            if (event.type == sf::Event::KeyPressed && (event.key.code == sf::Keyboard::Q || event.key.code == sf::Keyboard::Escape))
-                window.close();
+            }
+            key_pressed = event.type == sf::Event::KeyPressed;
+            if (key_pressed) {
+                if (event.key.code == sf::Keyboard::Q || event.key.code == sf::Keyboard::Escape) {
+                    window.close();
+                }
+            }
         }
 
         window.clear();
