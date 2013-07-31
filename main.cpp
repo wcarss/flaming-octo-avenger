d95ee30024f27d7c57bac1370da1557fb43d6fb8
Wyatt Carss <carss.w@gmail.com>
2013-07-31 02:35:39 -0400

    Kind of ridiculous smoothish movement implementation.

--- main.cpp
+++ main.cpp
@@ -4,59 +4,51 @@
 using namespace sf;
 using namespace std;
 
-float increase_y_velocity(float y_velocity);
-float decrease_y_velocity(float y_velocity);
-float increase_x_velocity(float x_velocity);
-float decrease_x_velocity(float x_velocity);
+void increase_y_velocity(float& y_velocity);
+void decrease_y_velocity(float& y_velocity);
+void increase_x_velocity(float& x_velocity);
+void decrease_x_velocity(float& x_velocity);
 void update_position(CircleShape& shape, Vector2f velocity);
 void velocity_falloff(Vector2f& velocity);
 
-float increase_y_velocity(float y_velocity)
+void increase_y_velocity(float& y_velocity)
 {
     float y_acceleration = 1.5;
     float max_y_velocity = 20;
     y_velocity += y_acceleration;
-cout<<"y_velocity: "<<y_velocity<<", max_y_velocity: "<<max_y_velocity<<endl;
     if (y_velocity > max_y_velocity) {
         y_velocity = max_y_velocity;
     }
-    return y_velocity;
 }
 
-float decrease_y_velocity(float y_velocity)
+void decrease_y_velocity(float& y_velocity)
 {
     float y_acceleration = 1.5;
-    float min_y_velocity = -20;
-    y_velocity = y_velocity - y_acceleration;
-cout<<"y_velocity: "<<y_velocity<<", -max_y_velocity: "<< min_y_velocity <<endl;
-    if (y_velocity < min_y_velocity) {
-        y_velocity = min_y_velocity;
+    float max_y_velocity = 20;
+    y_velocity -= y_acceleration;
+    if (y_velocity < -max_y_velocity) {
+        y_velocity = -max_y_velocity;
     }
-    return y_velocity;
 }
 
-float increase_x_velocity(float x_velocity)
+void increase_x_velocity(float& x_velocity)
 {
     float x_acceleration = 1.5;
     float max_x_velocity = 20;
     x_velocity += x_acceleration;
-cout<<"x_velocity: "<<x_velocity<<", max_x_velocity: "<<max_x_velocity<<endl;
     if (x_velocity > max_x_velocity) {
         x_velocity = max_x_velocity;
     }
-    return x_velocity;
 }
 
-float decrease_x_velocity(float x_velocity)
+void decrease_x_velocity(float& x_velocity)
 {
     float x_acceleration = 1.5;
     float max_x_velocity = 20;
     x_velocity -= x_acceleration;
-cout<<"x_velocity: "<<x_velocity<<", -max_x_velocity: "<< -1 * max_x_velocity <<endl;
-    if (x_velocity < -1 * max_x_velocity) {
-        x_velocity = -1 * max_x_velocity;
+    if (x_velocity < -max_x_velocity) {
+        x_velocity = -max_x_velocity;
     }
-    return x_velocity;
 }
 
 void update_position(CircleShape& shape, Vector2f velocity) {
@@ -64,9 +56,9 @@ void update_position(CircleShape& shape, Vector2f velocity) {
     float max_x_position = 800;
     float min_y_position = 0;
     float max_y_position = 600;
-    Vector2f current_position, new_position;
-    current_position = shape.getPosition();
-    new_position = current_position + velocity;
+cout<<"1 shape is ("<<shape.getPosition().x<<","<<shape.getPosition().y<<")"<<endl;
+cout<<"velocity is ("<<velocity.x<<","<<velocity.y<<")"<<endl;
+    Vector2f new_position(shape.getPosition() + velocity);
 
     if (new_position.x > max_x_position) {
         new_position.x = max_x_position;
@@ -77,21 +69,23 @@ void update_position(CircleShape& shape, Vector2f velocity) {
     if (new_position.y > max_y_position) {
         new_position.y = max_y_position;
     }
-    if (new_position.y < max_y_position) {
+    if (new_position.y < min_y_position) {
         new_position.y = min_y_position;
     }
 
+cout<<"2 shape is ("<<shape.getPosition().x<<","<<shape.getPosition().y<<")"<<endl;
     shape.setPosition(new_position);
+cout<<"3 shape is ("<<shape.getPosition().x<<","<<shape.getPosition().y<<")"<<endl;
 }
 
 void velocity_falloff(Vector2f& velocity) {
-    Vector2f falloff(1.f, 1.f);
+    Vector2f falloff(1.1f, 1.1f);
     float velocity_epsilon = 0.01f;
     velocity.x /= falloff.x;
     velocity.y /= falloff.y;
-    if (velocity.x < velocity_epsilon)
+    if (velocity.x > -velocity_epsilon && velocity.x < velocity_epsilon)
         velocity.x = 0;
-    if (velocity.y < velocity_epsilon)
+    if (velocity.y > -velocity_epsilon && velocity.y < velocity_epsilon)
         velocity.y = 0;
 }
 
@@ -103,7 +97,7 @@ int main()
     int key_code;
     Vector2f velocity(0,0);
     shape.setFillColor(Color::Green);
-    shape.setPosition(200.0f, -200.0f);
+    shape.setPosition(200.0f, 200.0f);
 
     while (window.isOpen())
     {
@@ -120,24 +114,16 @@ int main()
                     window.close();
                 }
                 if (key_code == Keyboard::W) {
-cout<<"w: "<<velocity.y<<endl;
-                    velocity.y = increase_y_velocity(velocity.y);
-cout<<"w: "<<velocity.y<<endl;
+                    increase_y_velocity(velocity.y);
                 }
                 if (key_code == Keyboard::S) {
-cout<<"s: "<<velocity.y<<endl;
-                    velocity.y = decrease_y_velocity(velocity.y);
-cout<<"s: "<<velocity.y<<endl;
+                    decrease_y_velocity(velocity.y);
                 }
                 if (key_code == Keyboard::A) {
-cout<<"a: "<<velocity.x<<endl;
-                    velocity.x = decrease_x_velocity(velocity.x);
-cout<<"a: "<<velocity.x<<endl;
+                    decrease_x_velocity(velocity.x);
                 }
                 if (key_code == Keyboard::D) {
-cout<<"d: "<<velocity.x<<endl;
-                    velocity.x = increase_x_velocity(velocity.x);
-cout<<"d: "<<velocity.x<<endl;
+                    increase_x_velocity(velocity.x);
                 }
             }
         }
