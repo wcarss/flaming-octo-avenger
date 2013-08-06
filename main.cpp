765c0a1ea5d0df536e12f2c7ca512c7795e09262
Wyatt Carss <carss.w@gmail.com>
2013-08-05 21:48:23 -0400

    Add rudimentary object intersection.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,35 @@
 using namespace sf;
 using namespace std;
 
+class Object {
+public:
+    bool passable;
+    Texture texture;
+    Sprite sprite;
+    FloatRect rect;
+
+    Vector2f setSize(Vector2f size) {
+        FloatRect current_bounds = this->sprite.getGlobalBounds();
+//        cout<<"current bounds: pos: ("<<current_bounds.left<<", "<<current_bounds.top<<"), size: ("<<current_bounds.width<<","<<current_bounds.height<<")."<<endl;
+        float scale_x = size.x / current_bounds.width;
+        float scale_y = size.y / current_bounds.height;
+        this->sprite.scale(scale_x, scale_y);
+        current_bounds = this->sprite.getGlobalBounds();
+//        cout<<"current bounds: pos: ("<<current_bounds.left<<", "<<current_bounds.top<<"), size: ("<<current_bounds.width<<","<<current_bounds.height<<")."<<endl;
+    }
+
+    Object(String str, Vector2f pos, Vector2f size, bool passable) {
+        this->texture.loadFromFile(str);
+        this->texture.setRepeated(true);
+        this->sprite.setTexture(this->texture);
+        this->sprite.setPosition(pos);
+        setSize(size);
+        //this->sprite.setScale(this->sprite.getGlobalBounds();
+        this->passable = passable;
+        this->rect = FloatRect(pos, size);
+    }
+};
+
 class Player {
 public:
     Vector2f max_velocity;
@@ -12,18 +41,27 @@ public:
     Vector2f velocity;
     Texture texture;
     Sprite sprite;
+    Vector2f size;
 
-    Player(float size){
-        setPosition(200.f, 200.f);
-        max_velocity = Vector2f(15.f, 15.f);
-        min_velocity = Vector2f(-15.f, -15.f);
-        acceleration = 1.1f;
+    Player(float size_x, float size_y) {
+        FloatRect bounds_rect;
+        this->max_velocity = Vector2f(15.f, 15.f);
+        this->min_velocity = Vector2f(-15.f, -15.f);
+        this->acceleration = 1.1f;
         if (!texture.loadFromFile("player.png")) {
             cerr<<"Failed to load texture."<<endl;
         }
-        texture.setSmooth(true);
-        sprite.setTexture(texture);
-        sprite.setColor(Color::Green);
+        this->texture.setSmooth(true);
+        this->sprite.setTexture(texture);
+        this->sprite.setColor(Color::Green);
+        bounds_rect = sprite.getGlobalBounds();
+//        cout<<"current bounds: pos: ("<<bounds_rect.left<<", "<<bounds_rect.top<<"), size: ("<<bounds_rect.width<<","<<bounds_rect.height<<")."<<endl;
+        this->size = Vector2f(size_x, size_y);
+        this->sprite.scale(this->size.x / bounds_rect.height, this->size.y / bounds_rect.width);
+        //this->sprite.setOrigin(this->size.x / 2, this->size.y / 2);
+        bounds_rect = sprite.getGlobalBounds();
+//        cout<<"current bounds: pos: ("<<bounds_rect.left<<", "<<bounds_rect.top<<"), size: ("<<bounds_rect.width<<","<<bounds_rect.height<<")."<<endl;
+        setPosition(200.f, 200.f);
     }
 
     void setPosition(float x, float y) {
@@ -74,11 +112,11 @@ public:
         }
     }
 
-    void update_position() {
+    void update_position(Object object) {
         float min_x_position = 0;
         float max_x_position = 700;
         float min_y_position = 0;
-        float max_y_position = 500;
+        float max_y_position = 600;
 
         velocity_falloff(velocity);
         Vector2f new_position(sprite.getPosition() + velocity);
@@ -94,6 +132,14 @@ public:
         }
         if (new_position.y < min_y_position) {
             new_position.y = min_y_position;
+        }
+
+//cout<<"object.passable: "<<object.passable<<"."<<endl;
+//cout<<"object.rect: pos: ("<<object.rect.left<<", "<<object.rect.top<<"), size: ("<<object.rect.width<<","<<object.rect.height<<")."<<endl;
+//cout<<"new_position: ("<<new_position.x<<", "<<new_position.y<<")."<<endl;
+        if (!object.passable && object.rect.intersects(FloatRect(new_position, size))) {
+//cout<<"CONTAINS!!!"<<endl;
+            new_position = sprite.getPosition();
         }
 
         sprite.setPosition(new_position);
@@ -114,11 +160,12 @@ public:
 int main()
 {
     RenderWindow window(VideoMode(800, 600), "Flaming Octo Avenger!");
-    Player player(50.f);
-    RectangleShape rectangle(Vector2f(800.f, 50.f));
+    Player player(100.f, 100.f);
+    Object wall("brown.png", Vector2f(0.0f, 550.0f), Vector2f(800.f, 50.f), false);
+    //RectangleShape rectangle(Vector2f(800.f, 50.f));
     int key_code;
-    rectangle.setFillColor(Color(100,80,80));
-    rectangle.setPosition(0.0f, 550.0f);
+    //rectangle.setFillColor(Color(100,80,80));
+    //rectangle.setPosition(0.0f, 550.0f);
 
     while (window.isOpen())
     {
@@ -141,10 +188,10 @@ int main()
             }
         }
         player.controls();
-        player.update_position();
+        player.update_position(wall);
 
         window.clear(Color(30,30,30));
-        window.draw(rectangle);
+        window.draw(wall.sprite);
         player.draw(window);
         window.display();
     }
